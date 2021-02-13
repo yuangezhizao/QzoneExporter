76a21f7e821960e6f6d3fb552601161fd5511406
yuangezhizao-MacMini <yuangezhizao@icloud.com>
2021-02-13 18:57:40 +0800

    💬 Texts(__init__.py): 沿用旧代码

---
+++ __init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env/ python3
+# -*- coding: utf-8 -*-
+"""
+    :Author: wwwpf & yuangezhizao
+    :Time: 2021/2/13 18:54
+    :Site: https://www.yuangezhizao.cn
+    :Copyright: © 2020~2021 yuangezhizao <root@yuangezhizao.cn>
+"""
